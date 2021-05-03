b46c1281206a0ad3f6c163caf8874dfbb1a7c48c
Jean-Baptiste LAFON <snooze.dev@protonmail.com>
2021-05-03 17:01:30 +0200

    Added single minified JS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 const path = require('path')
+const fs = require('fs')
 
 module.exports = {
+  future: {
+    webpack5: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  env: {
+    js: fs.readFileSync('./public/js/scripts.min.js').toString()
+  }
 }
--- taskfile.js
+++ taskfile.js
@@ -1,4 +1,19 @@
-exports.copySass = function * (task) {
+exports.copyAssets = function * (task) {
     yield task.source('node_modules/aos/src/sass/*.scss').target('styles/theme/assets/aos/');
+    yield task.source('node_modules/aos/dist/*.js').target('src/js/');
     yield task.source('node_modules/bootstrap/scss/**/*.scss').target('styles/theme/assets/bootstrap-src/');
+}
+
+exports.minifyJS = function * (task) {
+    yield task.source('src/js/*.js')
+    .concat('scripts.min.js')
+    .uglify({
+        annnotations: false,
+        compress: {
+            drop_console: true,
+            join_vars: true
+        },
+        sourceMap: true
+      })
+    .target('./public/js/');
 }
