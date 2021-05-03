47416c45d48b87a79145b1a1b92c6e66b6e3e98a
Jean-Baptiste LAFON <snooze.dev@protonmail.com>
2021-05-03 13:08:39 +0200

    Added Bootstrap 5

--- taskfile.js
+++ taskfile.js
@@ -1,3 +1,4 @@
 exports.copySass = function * (task) {
-    yield task.source('node_modules/aos/src/sass/*.scss').target('styles/theme/assets/aos/')
-  }
+    yield task.source('node_modules/aos/src/sass/*.scss').target('styles/theme/assets/aos/');
+    yield task.source('node_modules/bootstrap/scss/**/*.scss').target('styles/theme/assets/bootstrap-src/');
+}
