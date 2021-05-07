9d4e403090c51704231bb04d7989ad57dd6a1ff5
Jean-Baptiste LAFON <snooze.dev@protonmail.com>
2021-05-07 11:19:11 +0200

    Tutorial 2.9
    https://nextjs.org/learn/basics/assets-metadata-css/styling-tips

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,18 +1,20 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import styles from '../../styles/Landing.module.scss'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
 
 export default function FirstPost() {
-
-  let array = [...Array(5)]
-
   return (
-    <>
-      <header className={styles.header}>
-        <div>{array.map(_=><p className={styles.test}>SNOOZESTRAP</p>)}</div>
-        <div><h1>SNOOZESTRAP</h1></div>
-        <div>{array.map(_=><p className={styles.test}>SNOOZESTRAP</p>)}</div>
-      </header>
-    </>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
   )
 }
